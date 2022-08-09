2f20695e644192c5a23b74fa1ea263e0804e164b
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-08 22:48:41 -0300

    classe user

---
+++ src/main/java/br/com/rafaelmartins/api/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.rafaelmartins.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
