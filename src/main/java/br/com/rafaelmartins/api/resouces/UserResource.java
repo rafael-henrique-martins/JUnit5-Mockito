e43219bd4b22c80175a9fabb6f0794a522e3b117
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-09 00:07:22 -0300

    interface user controller findbyid

--- src/main/java/br/com/rafaelmartins/api/resouces/UserResource.java
+++ src/main/java/br/com/rafaelmartins/api/resouces/UserResource.java
@@ -1,8 +1,6 @@
 package br.com.rafaelmartins.api.resouces;
 
 import br.com.rafaelmartins.api.domain.User;
-import br.com.rafaelmartins.api.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(userService.findById(id));
+        return ResponseEntity.ok().body(new User(1, "Rafael", "rafael@hotmail", "123"));
     }
 }
