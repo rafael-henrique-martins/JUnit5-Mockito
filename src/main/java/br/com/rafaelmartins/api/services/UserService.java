10096baa380c8ad6cbc502caaa7c4a1835fdc6dd
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-09 22:21:00 -0300

    CRUD para user

--- src/main/java/br/com/rafaelmartins/api/services/UserService.java
+++ src/main/java/br/com/rafaelmartins/api/services/UserService.java
@@ -1,8 +1,16 @@
 package br.com.rafaelmartins.api.services;
 
+
 import br.com.rafaelmartins.api.domain.User;
+import br.com.rafaelmartins.api.domain.dto.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
+    User create(UserDTO obj);
+    User update(UserDTO obj);
+    void delete(Integer id);
 }
