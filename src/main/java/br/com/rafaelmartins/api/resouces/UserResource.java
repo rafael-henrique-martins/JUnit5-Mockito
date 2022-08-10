617f89141dbfd21dce96ad6be80f7abcb38eb247
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-09 22:09:44 -0300

    Retornando DTO para UserResource

--- src/main/java/br/com/rafaelmartins/api/resouces/UserResource.java
+++ src/main/java/br/com/rafaelmartins/api/resouces/UserResource.java
@@ -1,22 +1,57 @@
 package br.com.rafaelmartins.api.resouces;
 
-import br.com.rafaelmartins.api.domain.User;
+
+import br.com.rafaelmartins.api.domain.dto.UserDTO;
 import br.com.rafaelmartins.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    private static final String ID = "/{id}";
+
     @Autowired
-    private UserService userService;
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(userService.findById(id));
+    private ModelMapper mapper;
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = ID)
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path(ID).buildAndExpand(service.create(obj).getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = ID)
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
+    }
+
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
